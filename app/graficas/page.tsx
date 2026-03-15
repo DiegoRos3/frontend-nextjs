2b6c3861dc72362120dc9c54417861829be579db
Jonathan David Salazar Vazquez <l22550321@chihuahua2.tecnm.mx>
2026-03-15 17:37:04 -0600

    KPIs agregadas
    Se agregaron 3 KPIs para mostrar datos de los datos procesados, por ahora falta darles algo de estilos

--- app/graficas/page.tsx
+++ app/graficas/page.tsx
@@ -1,11 +1,14 @@
 import { AppHeader } from "@/components/app-header";
+import KpiCard from "@/components/kpi-card";
 
 export default function Graficas() {
     return (
         <>
             <AppHeader title="Graficas" />
-            <div className="p-4">
-                <h1>Visualización de Datos</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3.5 p-6">
+                    <KpiCard nombreDato="awa" valor="126 no se que"/>
+                    <KpiCard nombreDato="owo" valor="Una feria"/>
+                    <KpiCard nombreDato="uwu" valor="Ingrese el valor ヾ(•ω•`)o"/>
             </div>
         </>
     )
---
+++ components/kpi-card.tsx
@@ -0,0 +1,22 @@
+import { ChartNoAxesCombined } from "lucide-react";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+
+type KpiCradProps = {
+    nombreDato: string,
+    valor: string
+}
+
+export default function KpiCard(datos: KpiCradProps){
+    return (
+        <Card size="default" className="mx-auto w-full max-w-sm sm:grid-cols-1">
+            <CardHeader className="flex justify-items-start gap-3">
+                <CardTitle className="text-3xl">{datos.nombreDato}</CardTitle>
+                <ChartNoAxesCombined size={25} className="bg-emerald-600/20 text-emerald-600 dark:bg-emerald-600/20 dark:text-emerald-600 rounded-md"/>
+            </CardHeader>
+            <CardContent>
+                <p>{datos.valor}</p>
+            </CardContent>
+        </Card>
+    )
+    
+}
