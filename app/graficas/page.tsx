ca10fd7285220b5eb4f0e58b4bd2d16eb9aa37d2
Diego Rose <l22550309@chihuahua2.tecnm.mx>
2026-03-23 15:05:41 -0600

    grafica de los cuatrimestres

--- app/graficas/page.tsx
+++ app/graficas/page.tsx
@@ -3,19 +3,37 @@
 import { AppHeader } from "@/components/app-header";
 import KpiCard from "@/components/kpi-card";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { ChartBarMultiple } from "@/components/chart-bar-multiple";
 
 export default function Graficas() {
 
     const dashboardData = useAppSelector((state) => state.dashboardData.data);
     const dispatch = useAppDispatch();
 
+    if (dashboardData.importe_margen_cuatrimestre.length === 0) {
+        return (
+            <>
+                <AppHeader title="Graficas" />
+                <div className="p-8 max-w-4xl mx-auto space-y-12 text-center">
+                    <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
+                        No se pudieron analizar tus datos
+                    </h1>
+                    <p className="text-xl text-muted-foreground mx-auto max-w-2xl">
+                        Intenta subir de nuevo tu archivo.
+                    </p>
+                </div>
+            </>
+        )
+    }
+
     return (
         <>
             <AppHeader title="Graficas" />
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3.5 p-6">
-                    <KpiCard nombreDato="awa" valor={dashboardData.importe_total}/>
-                    <KpiCard nombreDato="owo" valor={dashboardData.ganancia_bruta}/>
-                    <KpiCard nombreDato="uwu" valor={dashboardData.kilos_vendidos}/>
+                <KpiCard nombreDato="Importe Total" valor={dashboardData.importe_total} />
+                <KpiCard nombreDato="Ganancia Bruta" valor={dashboardData.ganancia_bruta} />
+                <KpiCard nombreDato="Kilos Vendidos" valor={dashboardData.kilos_vendidos} />
+                <ChartBarMultiple data={dashboardData.importe_margen_cuatrimestre} />
             </div>
         </>
     )
---
+++ components/chart-bar-multiple.tsx
@@ -0,0 +1,72 @@
+"use client"
+
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts"
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import {
+    ChartContainer,
+    ChartTooltip,
+    ChartTooltipContent,
+    type ChartConfig,
+} from "@/components/ui/chart"
+import { ImporteMargenCuatrimestre } from "@/app/store/slices/dataSlice"
+
+export const description = "A multiple bar chart para importe y margen por cuatrimestre"
+
+const chartConfig = {
+    IMPORTE_TOTAL: {
+        label: "Importe Total (MXN) ",
+        color: "var(--chart-1)",
+    },
+    MARGEN_TOTAL: {
+        label: "Margen Total (MXN) ",
+        color: "var(--chart-2)",
+    },
+} satisfies ChartConfig
+
+interface ChartBarMultipleProps {
+    data: ImporteMargenCuatrimestre[]
+}
+
+export function ChartBarMultiple({ data }: ChartBarMultipleProps) {
+    return (
+        <Card size="default" className="mx-auto w-full max-w-sm">
+            <CardHeader>
+                <CardTitle>Importe y Margen</CardTitle>
+                <CardDescription>Por Cuatrimestre</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <ChartContainer config={chartConfig}>
+                    <BarChart accessibilityLayer data={data}>
+                        <CartesianGrid vertical={false} />
+                        <XAxis
+                            dataKey="CUATRIMESTRE"
+                            tickLine={false}
+                            tickMargin={10}
+                            axisLine={false}
+                            tickFormatter={(value) => `Q${value}`}
+                        />
+                        <ChartTooltip
+                            cursor={false}
+                            content={<ChartTooltipContent indicator="dashed" />}
+                        />
+                        <Bar dataKey="IMPORTE_TOTAL" fill="var(--color-IMPORTE_TOTAL)" radius={4} />
+                        <Bar dataKey="MARGEN_TOTAL" fill="var(--color-MARGEN_TOTAL)" radius={4} />
+                    </BarChart>
+                </ChartContainer>
+            </CardContent>
+            <CardFooter className="flex-col items-start gap-2 text-sm">
+                <div className="flex gap-2 leading-none font-medium text-muted-foreground">
+                    Comparativa de Importe vs Margen por cuatrimestre
+                </div>
+            </CardFooter>
+        </Card>
+    )
+}
